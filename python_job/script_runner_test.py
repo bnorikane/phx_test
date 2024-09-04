a4885ed08d7eb9fc2e466d554ebd97aa131e9d14
bnorikane <bnorikane@msn.com>
2024-09-04 09:58:49 -0600

    Add load_to_bq()
    - add pct_area.csv to repo
    - load pct_area.csv to bq table

--- python_job/script_runner_test.py
+++ python_job/script_runner_test.py
@@ -1,5 +1,22 @@
 # python script to test Portal Script Runner job
 # Based on DNC Portal Script Runner training
 
-# 
+from google.cloud import bigquery
+
+client = bigquery.Client(project='CO Party')
+
+# create table in BigQuery
+
+def load_to_bq():
+    rescsv = open('pct_area.csv', 'rb')  # NOTE: rb mode
+    client.load_table_from_file(rescsv, 'CO Party.sbx_norikaneb.script_test')
+
+
+# Check script arguments and dump for debug run
+# if len(sys.argv) > 1:
+#     if sys.argv[1] == 'dump':
+#         dump_pdf()
+# else:
+#     #run job
+load_to_bq()
 
